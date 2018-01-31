28345f43628816aeceae15e03be7bc0b0ac0ce7d
balajidhoni <35687870+balajidhoni@users.noreply.github.com>
2018-01-31 14:37:42 +0530

    negative or positive
    c program

---
+++ balaji.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,d;
+clrscr();
+printf("\n the positive or negative number");
+scanf("\n%d%d",&a,&b);
+if(a>b)
+{
+printf("\n the number is positive");
+}
+else
+{
+printf("the number is negative");
+}
+getch();
+}
